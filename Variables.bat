7674629b5d604fe671e9808f5f65e372fea4ede0
naveenb2004 <naveennbalasooriya2004@gmail.com>
2022-02-18 15:00:21 +0530

    bug fix

--- Variables.bat
+++ Variables.bat
@@ -7,8 +7,6 @@ set path=C:\ProgramData\HotspotMakerData
 set/p version=<"%nowpath%\Version.ini"
 ::window title
 set title=Hotspot Maker (v%version%)
-::check and remove update leftovers
-if EXIST "%path%\updateinstaller.bat" set leftovers=found &del "%path%\updateinstaller.bat"
 ::check and import color
 if EXIST "%path%\colorcode.ini" goto WIZARDSTART
 if NOT EXIST "%path%\colorcode.ini" goto COLORMAKE
@@ -33,7 +31,6 @@ SET CREDIT5=                 ***                 - Open Source Project -        
 SET CREDIT6=                 ***                                                        ***
 SET CREDIT7=                 **************************************************************
 SET CREDIT8=                 --------------------------------------------------------------
-echo [%date%] [%time%] set interface credits >>"%path%\Logger.log"
 ::check default username and password
 if EXIST "%path%\defuser.nnb" call "%nowpath%\Home.bat"
 if NOT EXIST "%path%\defuser.nnb" call "%nowpath%\SaveCredits.bat"
