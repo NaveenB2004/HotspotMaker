0a9739f02d7799f1669ec8e7793e58345ad6ba2b
naveenb2004 <naveennbalasooriya2004@gmail.com>
2022-06-16 23:21:22 +0530

    2.6 updates

--- Hotspot Maker - Batch/Sizes.bat
+++ Hotspot Maker - Batch/Sizes.bat
@@ -6,23 +6,21 @@ title %title%
 ::credits
 echo %CREDIT0%&echo %CREDIT1%&echo %CREDIT2%&echo %CREDIT4%&echo %CREDIT5%&echo %CREDIT6%&echo %CREDIT7%&echo %CREDIT8%
 ::tab title
-echo [ Window and Buffer Resizer ]
+echo [ Window Resizer ]
 echo.
-echo Welcome to window and buffer sizes changer!
+echo Welcome to window sizes changer!
 echo Please choose the below options for goto resizers and instructions!
 echo.
 ::user choicess
-echo A - Change Window Size&echo B - Change Buffer Size&echo C - Go Back&echo D - Exit
+echo A - Change Window Size&echo B - Go Back&echo C - Exit
 echo.
 set/p "sizecho=>"
 if %sizecho%==A goto WINDOWSIZE
 if %sizecho%==a goto WINDOWSIZE
-if %sizecho%==B goto BUFFERSIZE
-if %sizecho%==b goto BUFFERSIZE
-if %sizecho%==C call "%nowpath%\Details.bat"
-if %sizecho%==c call "%nowpath%\Details.bat"
-if %sizecho%==D call "%nowpath%\Exit.bat"
-if %sizecho%==d call "%nowpath%\Exit.bat"
+if %sizecho%==B call "%nowpath%\Details.bat"
+if %sizecho%==b call "%nowpath%\Details.bat"
+if %sizecho%==C call "%nowpath%\Exit.bat"
+if %sizecho%==c call "%nowpath%\Exit.bat"
 echo invalid choice... Try again...
 %timeout% 6
 goto SIZES
@@ -48,8 +46,8 @@ echo.
 echo A - Apply&echo B - Save&echo C - Back&echo D - Exit
 echo.
 set/p "sizecho=>"
-if %sizecho%==A goto WINAPPLY
-if %sizecho%==a goto WINAPPLY
+if %sizecho%==A goto WINSIZEAPPLY
+if %sizecho%==a goto WINSIZEAPPLY
 if %sizecho%==B goto WINSAVE
 if %sizecho%==b goto WINSAVE
 if %sizecho%==C goto SIZES
@@ -60,35 +58,18 @@ echo invalid choice... Try again...
 %timeout% 6
 goto WINDOWSIZE
 
-:BUFFERSIZE
-::clear before outputs
-cls
-::title
-title %title%
-::credits
-echo %CREDIT0%&echo %CREDIT1%&echo %CREDIT2%&echo %CREDIT4%&echo %CREDIT5%&echo %CREDIT6%&echo %CREDIT7%&echo %CREDIT8%
-::tab title
-echo [ Window Resizer ]
-echo.
-echo Add values and select option apply to review changes. The option 'Apply' will not save changes permenantly.
-echo After you decided the values, select option 'Save' to set changes permanantly.
-echo.
-echo Window Size Changer
-set/p "bufferwidth=Buffer Width: "
-set/p "bufferheight=Buffer Height: "
-::user choicess
-echo.
-echo A - Apply&echo B - Save&echo C - Back&echo D - Exit
-echo.
-set/p "sizecho=>"
-if %sizecho%==A goto BUFFERAPPLY
-if %sizecho%==a goto BUFFERAPPLY
-if %sizecho%==B goto BUFFERSAVE
-if %sizecho%==b goto BUFFERSAVE
-if %sizecho%==C goto SIZES
-if %sizecho%==c goto SIZES
-if %sizecho%==D call "%nowpath%\Exit.bat"
-if %sizecho%==d call "%nowpath%\Exit.bat"
-echo invalid choice... Try again...
-%timeout% 6
-goto BUFFERSIZE
+:WINSIZEAPPLY
+echo @Echo off >"%path%\temp-size.bat"
+echo echo Loading Windo Sizes... >>"%path%\temp-size.bat"
+echo echo Please Wait... >>"%path%\temp-size.bat"
+echo echo. >>"%path%\temp-size.bat"
+echo echo Window Width: %winwidth% >>"%path%\temp-size.bat"
+echo echo Window Height: %winheight% >>"%path%\temp-size.bat"
+echo echo. >>"%path%\temp-size.bat"
+echo set powershell=%SYSTEMROOT%\System32\WindowsPowerShell\v1.0\powershell.exe >>"%path%\temp-size.bat"
+echo %powershell% -command "&{$w=(get-host).ui.rawui;$w.buffersize=@{width=%winwidth%;height=500};$w.windowsize=@{width=%winwidth%;height=%winheight%};}" >>"%path%\temp-size.bat"
+echo pause >>"%path%\temp-size.bat"
+call "%path%\temp-size.bat"
+%timeout% 10 >nul
+del "%path%\temp-size.bat"
+goto WINDOWSIZE
