24f2ed9cd8199226f30af90816b3b80961b56e86
NaveenB2004 <naveennbalasooriya2004@gmail.com>
2023-08-04 02:53:49 +0530

    extensions db connected

--- Application/src/main/java/Advanced/Extensions/Database.java
+++ Application/src/main/java/Advanced/Extensions/Database.java
@@ -11,15 +11,16 @@ import java.util.logging.Logger;
  * @author NaveenB2004
  */
 public class Database {
-    
+
     public static Connection conn() {
         Connection conn = null;
         try {
-            conn = DriverManager.getConnection("jdbc:sqlite::resource:http://");
+            conn = DriverManager.getConnection("jdbc:sqlite::resource:"
+                    + "https://github.com/NaveenB2004/HotspotMaker/blob/main/Others/Extensions.db");
         } catch (SQLException ex) {
             Logger.getLogger(Database.class.getName()).log(Level.SEVERE, null, ex);
         }
         return conn();
     }
-    
+
 }
