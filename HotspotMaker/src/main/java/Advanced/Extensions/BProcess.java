dd67fc69c51f30685bace098c1ce13e618f7e728
NaveenB2004 <naveennbalasooriya2004@gmail.com>
2023-08-06 12:55:31 +0530

    updates

--- HotspotMaker/src/main/java/Advanced/Extensions/BProcess.java
+++ HotspotMaker/src/main/java/Advanced/Extensions/BProcess.java
@@ -1,19 +1,24 @@
 package Advanced.Extensions;
 
+import Main.Settings;
 import java.awt.Desktop;
 import java.awt.Frame;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.MalformedURLException;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.stream.Stream;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
@@ -70,7 +75,7 @@ public class BProcess {
                 version.setText(rs.getString("version"));
                 release.setText(rs.getString("release"));
                 date.setText(rs.getString("date"));
-                
+
                 status.setText(getStatus(version.getText()));
 
                 sourceLink = rs.getString("source");
@@ -149,17 +154,20 @@ public class BProcess {
                     setActions("Downloading...");
                     FileUtils.copyURLToFile(new URL(directLink),
                             new File(extDir + "tmp\\ext-" + extId + ".zip"));
+                    
+                    setActions("Checking Extractor...");
+                    winrar();
 
                     setActions("Installing...");
                     new ProcessBuilder("cmd.exe", "/c",
                             "\"" + winrar + "\" "
-                            + "x "
+                            + "x -o+ -ibck "
                             + extDir + "tmp\\ext-" + extId + ".zip "
                             + extDir + "ext-" + extId).start();
 
                     setActions("Finishing...");
                     FileUtils.deleteDirectory(new File(extDir + "tmp\\ext-" + extId + ".zip"));
-                    getStatus(version.getText());
+                    status.setText(getStatus(version.getText()));
 
                     btnEnable();
 
@@ -175,26 +183,63 @@ public class BProcess {
         }).start();
     }
 
+    private void winrar() {
+        if (!new File(winrar).exists()){
+            try {
+                FileUtils.copyURLToFile(new URL(""),
+                        new File(winrar));
+            } catch (MalformedURLException ex) {
+                Logger.getLogger(BProcess.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (IOException ex) {
+                Logger.getLogger(BProcess.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+    
     public void btnOpenClickEvt() {
 
     }
 
+    private String readStarter() {
+        String path = null;
+        if (new File(extDir + "ext-" + extId + "\\.starter").exists()) {
+            try (Stream<String> lines = Files.lines(
+                    Paths.get(extDir + "ext-" + extId + "\\.starter"))) {
+                path = lines.skip(0).findFirst().get();
+            } catch (IOException e) {
+                Logger.getLogger(BProcess.class.getName())
+                        .log(Level.SEVERE, null, e);
+            }
+        }
+        return path;
+    }
+
     private String getStatus(String version) {
         String rtnStatus = null;
-        try {
-            ProcessBuilder processBuilder = new ProcessBuilder("cmd.exe", "/c",
-            "");
-            processBuilder.redirectErrorStream(true);
-            Process p = processBuilder.start();
-            String line = null;
-            BufferedReader bufferedReader
-                    = new BufferedReader(new InputStreamReader(p.getInputStream()));
-            while ((line = bufferedReader.readLine()) != null) {
-                rtnStatus = line;
+        String mainApp = readStarter();
+        if (mainApp != null) {
+            try {
+                ProcessBuilder processBuilder = new ProcessBuilder("cmd.exe", "/c",
+                        extDir + "ext-" + extId + "\\" + mainApp + " -v");
+                processBuilder.redirectErrorStream(true);
+                Process p = processBuilder.start();
+                String line = null;
+                BufferedReader bufferedReader
+                        = new BufferedReader(new InputStreamReader(p.getInputStream()));
+                while ((line = bufferedReader.readLine()) != null) {
+                    rtnStatus = line;
+                }
+            } catch (IOException ex) {
+                Logger.getLogger(BProcess.class.getName())
+                        .log(Level.SEVERE, null, ex);
             }
-        } catch (IOException ex) {
-            Logger.getLogger(BProcess.class.getName())
-                    .log(Level.SEVERE, null, ex);
+            if (!version.equals(rtnStatus)) {
+                rtnStatus = "Update Available!";
+            } else {
+                rtnStatus = "Up-to-Date!";
+            }
+        } else {
+            rtnStatus = "Not Installed!";
         }
         return rtnStatus;
     }
